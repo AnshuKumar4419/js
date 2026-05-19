e5e9e8e852843b5ee9ca24d6c588f4ae61b507e4
AnshuKumar4419 <anshukumarocket@gmail.com>
2026-05-19 20:21:08 +0000

    container config

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "anshu@google.com"
+var accountPassword = "12345"
+accountCity = "Samastipur"
+
+accountEmail = "anshu.com"
+accountPassword = "123456"
+accountCity = "Noida"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
